f1a21623e1876a0ee57443e88ed8d313e1b0e7e4
jekyun-park <parkjekyun@gmail.com>
2022-04-14 01:41:36 +0900

    update alert,action sheet examples

--- project5/AlertExample/AlertExample/ViewController.swift
+++ project5/AlertExample/AlertExample/ViewController.swift
@@ -28,11 +28,34 @@ class ViewController: UIViewController {
             print("Ok Pressed")
         })
         
+        // cancel 액션은 하나만 사용할 수 있다.
         let cancelAction: UIAlertAction
         cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
         
         alertController.addAction(okAction)
         alertController.addAction(cancelAction)
+        
+        let handler: (UIAlertAction) -> Void
+        handler = { (action:UIAlertAction) in
+            print("action pressed \(action.title ?? "")")
+        }
+        
+        let someAction: UIAlertAction
+        someAction = UIAlertAction(title: "Some", style: UIAlertAction.Style.destructive, handler: handler)
+        
+        let anotherAction: UIAlertAction
+        anotherAction = UIAlertAction(title: "Another", style: UIAlertAction.Style.default, handler: handler)
+        
+        alertController.addAction(someAction)
+        alertController.addAction(anotherAction)
+        
+        // textField는 얼럿에만 사용, 액션시트에는 사용되지 않음
+        if alertController.preferredStyle == .alert {
+            alertController.addTextField { (field:UITextField) in
+                field.placeholder = "placeholder"
+                field.textColor = UIColor.blue
+            }
+        }
         
         self.present(alertController, animated: true) {
             print("Alert Controller shown")
