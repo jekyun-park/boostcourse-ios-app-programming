918831b37c5d59be424eaf7cf693c2e09326b7f8
jegyun-Park <parkjekyun@gmail.com>
2022-05-10 21:55:35 +0900

    [Project 5, BoxOffice] Complete CollectionView
    CollectionView Auto Layout Setting Complete

---
+++ project5/BoxOffice/BoxOffice/Components/MovieListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieListCollectionViewCell.swift
+//  BoxOffice
+//
+//  Created by 박제균 on 2022/05/10.
+//
+
+import UIKit
+
+class MovieListCollectionViewCell: UICollectionViewCell {
+    
+}
